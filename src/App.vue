33c2586bec0aebdd41a06fc8331b9a3057cb5f67
7-7sada <jedsada.chot@gmail.com>
2024-07-04 11:55:57 +0700

    homecard and card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
+import HomeCard from '@/components/HomeCard.vue'
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCard />
 </template>
